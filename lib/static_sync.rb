4bbf9d396006048cf1052b7302f9c1222de36d4e
nx <nx@nu-ex.com>
2010-06-04 14:33:57 -0400

    refactoring

--- lib/static_sync.rb
+++ lib/static_sync.rb
@@ -5,7 +5,15 @@ module StaticSync
   class << self
 
     def static_path
-      File.join('tmp', 'static')
+      'tmp/static'
+    end
+
+    def checksum_file
+      'checksums.yml'
+    end
+
+    def checksum_path
+      'tmp' + checksum_file
     end
 
     def render_pages
@@ -31,27 +39,6 @@ module StaticSync
       FileUtils.cp_r('public/.', static_path)
     end
 
-    def make_checksums
-      checksums = {}
-      Dir[static_path + '/**/*.*'].entries.each do |f|
-        checksums[f.gsub(/tmp\/static\//, '')] = Digest::MD5.hexdigest(File.read(f))
-      end
-      return checksums
-    end
-
-    def build_command_list(old_checksums, new_checksums)
-      commands = []
-      new_checksums.each_key do |k|
-        unless old_checksums.has_key?(k) and (old_checksums[k] == new_checksums[k])
-          commands << { :put => k }
-        end
-        old_checksums.delete(k)
-      end
-      old_checksums.each_key do |k|
-        commands << { :delete => k }
-      end
-      return commands
-    end
 
     def ftp_config
       YAML::load_file('config/ftp.yml')
@@ -59,74 +46,94 @@ module StaticSync
 
     def sync
       Net::FTP.open(ftp_config[:host], ftp_config[:user], ftp_config[:password]) do |ftp|
-        known_dirs = []
 
         ftp.chdir(ftp_config[:root]) if ftp_config[:root]
 
-        # Get current state of remote site by downloading
-        # the previous yaml of checksums
-        checksum_path = 'tmp/checksums.yml'
-        unless ftp.list('checksums.yml').empty?
-          ftp.get('checksums.yml', checksum_path)
+        old_checksums = []
+        unless ftp.list(checksum_file).empty?
+          # Get current state of remote site by downloading
+          # the previous yaml of checksums
+          ftp.get(checksum_file, checksum_path)
           old_checksums = YAML::load_file(checksum_path)
-        else
-          old_checksums = {}
         end
+
         new_checksums = make_checksums
-        command_list = build_command_list(old_checksums, new_checksums)
+        to_upload, to_delete = file_queues(old_checksums, new_checksums)
 
-        unless command_list.empty?
-          command_list.each do |instruction|
-            instruction.each do |command, file|
-              if command == :put
-                # Make a tree of directories that may or may not
-                # need to be created
-                dir_parts = file.split('/')
-                dir_parts = dir_parts[0, (dir_parts.length - 1)]
-                dirs = []
-                dir_parts.each do |dir_part|
-                  path = dirs.join('/')
-                  if dir_part != dir_parts.first
-                    path << '/'
-                  end
-                  path << dir_part
-                  dirs << path
-                end
+        if to_upload.empty? && to_delete.empty?
+          puts " ! Files already synced"
+          return
+        end
 
-                dirs.each do |dir|
-                  if !known_dirs.include?(dir)
-                    if ftp.list(dir).empty?
-                      puts " Making directory: #{dir}"
-                      ftp.mkdir(dir)
+        created_directories = []
+        to_upload.each do |file|
+          directory_tree(file).each do |dir|
+            unless created_directories.include?(dir)
+              if ftp.list(dir).empty?
+                puts " Making directory: #{dir}"
+                ftp.mkdir(dir)
 
-                      # Track created directories so we don't
-                      # have to create them again
-                      known_dirs << dir
-                    end
-                  end
-                end
-                puts " Uploading: #{file}"
-                local_file = File.join(static_path, file)
-                ftp.put(local_file, file)
-              else
-                puts " Deleting: #{file}"
-                ftp.delete(file)
+                # Track created directories so we don't
+                # have to create them again
+                created_directories << dir
               end
             end
           end
-
-          # Upload the new checksums
-          File.open(checksum_path, 'w') do |f|
-            f.write(new_checksums.to_yaml)
-          end
-          puts " Uploading new checksums."
-          ftp.put(checksum_path, 'checksums.yml')
-
-          puts " All done."
-        else
-          puts " ! Files already synced"
+          puts " Uploading: #{file}"
+          local_file = File.join(static_path, file)
+          ftp.put(local_file, file)
         end
+
+        to_delete.each do |file|
+          puts " Deleting: #{file}"
+          ftp.delete(file)
+        end
+
+        # Upload the new checksums
+        File.open(checksum_path, 'w') do |f|
+          f.write(new_checksums.to_yaml)
+        end
+        puts " Uploading new checksums."
+        ftp.put(checksum_path, checksum_file)
+
+        puts " All done."
       end
+    end
+
+    private
+
+    def make_checksums
+      checksums = {}
+      filenames = Dir[static_path + '/**/*.*'].entries
+      filenames.each do |f|
+        checksums[f.gsub(static_path, '')] = Digest::MD5.hexdigest(File.read(f))]}
+      end
+      checksums
+    end
+
+    def file_queues(old, current)
+      uploading = []
+      current.each_pair do |file, sum|
+        uploading << file if old[file] != sum
+        old.delete(file)
+      end
+      deleting = old.keys
+      [uploading, deleting]
+    end
+
+    # Make a tree of directories that may or may not
+    # need to be created
+    def directory_tree(file)
+      dir_parts = file.split('/')
+      dir_parts = dir_parts[0, (dir_parts.length - 1)]
+      dirs = []
+      dir_parts.each do |dir_part|
+        path = dirs.join('/')
+        path << '/' if dir_part != dir_parts.first
+        path << dir_part
+        dirs << path
+      end
+      dirs
     end
   end
 end
