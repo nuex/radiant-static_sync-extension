13494da0d9adbca07bede8625a094f9b2e735d48
nx <nx@nu-ex.com>
2010-06-17 14:34:37 -0400

    added gh url to extension

--- static_sync_extension.rb
+++ static_sync_extension.rb
@@ -1,7 +1,7 @@
 class StaticSyncExtension < Radiant::Extension
   version "0.1"
   description "Sync a static version of the Radiant site to a remote FTP account."
-  url ""
+  url "http://github.com/promptsite/radiant-static_sync-extension.git"
 
   def activate
   end
